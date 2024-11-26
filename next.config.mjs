79c8500e08f800e33b74fd45b3d149e3d7862a7f
Uğur Pekünsal <ugurpekunsal@gmail.com>
2024-11-25 22:44:31 -0500

    Add 404 and loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		unoptimized: true,
 		domains: ['images.ctfassets.net'],
+		formats: ['image/avif', 'image/webp'],
 	},
+	compress: true,
+	poweredByHeader: false,
 };
 
 export default nextConfig;
